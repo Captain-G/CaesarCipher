c2664d9fdf8cd3e0f3f8d2b5a1e89b7636eb543f
Captian-G <poppinpites7@gmail.com>
2022-09-11 14:21:23 +0300

    Second commit

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ def decrypt(my_text, my_shift):
 
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+            "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
             "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 direction = input("Enter 'encode' to encrypt, or 'decode' to decrypt : ").lower()
+
 if direction != "encode" or direction != "decode":
     print("Please enter a valid option : ")
     direction = input("Enter 'encode' to encrypt, or 'decode' to decrypt : ").lower()
